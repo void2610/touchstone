405397454e57d84f5f27e0440d9cde15e9961e8a
void2610 <izumisan2610@gmail.com>
2023-04-02 05:09:48 +0900

    攻撃完成

--- Assets/Scripts/Equipment/MeleeWeapon.cs
+++ Assets/Scripts/Equipment/MeleeWeapon.cs
@@ -15,35 +15,17 @@ namespace NEquipment
 
 		private float attackStartAngle = 0;
 
-		private bool isActiveCopy
-		{
-			set
-			{
-				//isActiveCopy = value;
-				if (isActive)
-				{
-					UpdateAttackStartAngle();
-				}
-			}
-			get
-			{
-				return isActiveCopy;
-			}
-		}
-
-
 
 
 		public override IEnumerator Action()
 		{
 
-
+			Debug.Log(angle);
 			attackStartTime = Time.time;
 			isActive = true;
-			isActiveCopy = true;
+			attackStartAngle = angle;
 			yield return new WaitForSeconds(activeTimeLength);
 			isActive = false;
-			isActiveCopy = false;
 			isCooling = true;
 			yield return new WaitForSeconds(coolTimeLength);
 			isCooling = false;
@@ -52,24 +34,18 @@ namespace NEquipment
 
 		private void AttackAnimation()
 		{
-			if (Mathf.Abs(angle) < 90)
+			if (Mathf.Abs(attackStartAngle) < 90)
 			{
-				angle -= animationCurve.Evaluate((Time.time - attackStartTime) / activeTimeLength) * attackDegree;
+				angle -= ((Time.time - attackStartTime) / activeTimeLength) * attackDegree;
 			}
 			else
 			{
-				angle += animationCurve.Evaluate((Time.time - attackStartTime) / activeTimeLength) * attackDegree;
+				angle += ((Time.time - attackStartTime) / activeTimeLength) * attackDegree;
 			}
 			var radian = angle * (Mathf.PI / 180);
 			transform.position = new Vector3(Mathf.Cos(radian) * moveRadius + 10, Mathf.Sin(radian) * moveRadius - 25, 0).normalized + playerPosition;
 			transform.eulerAngles = new Vector3(0f, 0f, angle - 45);
 			return;
-		}
-
-		private void UpdateAttackStartAngle()
-		{
-			attackStartAngle = angle;
-			Debug.Log("AttackStartAngle: " + attackStartAngle);
 		}
 
 		public virtual void Start()
@@ -80,7 +56,6 @@ namespace NEquipment
 			isCooling = false;
 			isEnable = true;
 			isActive = false;
-			isActiveCopy = false;
 			activeTimeLength = 1f;
 			attackPower = 1;
 			moveRadius = 60;
