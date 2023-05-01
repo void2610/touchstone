aa2f7bf300f77c8354968a607fc302014286af28
void2610 <izumisan2610@gmail.com>
2023-05-02 00:54:58 +0900

    guard

--- Assets/Scripts/Equipment/Skill/Guard.cs
+++ Assets/Scripts/Equipment/Skill/Guard.cs
@@ -22,16 +22,27 @@ namespace NEquipment
 		{
 			player.GetComponent<Player>().isInvincible = true;
 			player.GetComponent<Player>().isMovable = false;
+			player.GetComponent<Rigidbody2D>().velocity = new Vector3(0, player.GetComponent<Rigidbody2D>().velocity.y, 0);
+			if (player.transform.localScale.x > 0)
+			{
+				this.transform.position = player.transform.position + new Vector3(1.5f, -0.4f, 0);
+			}
+			else
+			{
+				this.transform.position = player.transform.position + new Vector3(-1.5f, -0.4f, 0);
+			}
 		}
 
 		public override void OnActionEnd()
 		{
 			player.GetComponent<Player>().isInvincible = false;
 			player.GetComponent<Player>().isMovable = true;
+			this.transform.position = new Vector3(0, 0, -10);
 		}
 
 		public override void Start()
 		{
+			this.transform.position = new Vector3(0, 0, -10);
 			base.Start();
 		}
 	}
