aa8f8a40a7f1146727d23321e0f612a4894d7508
void2610 <izumisan2610@gmail.com>
2023-05-02 17:31:23 +0900

    修正

--- Assets/Scripts/System/SetEquipmentScript.cs
+++ Assets/Scripts/System/SetEquipmentScript.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NEquipment;
 
 public class SetEquipmentScript : MonoBehaviour
 {
 	private string weapon = "Sword";
 	private string skill1 = "Grapple";
 	private string skill2 = "Dash";
+	private GameObject weaponObj = null;
+	private GameObject skill1Obj = null;
+	private GameObject skill2Obj = null;
 	void Start()
 	{
 		weapon = PlayerPrefs.GetString("NowEquipWeapon", "Sword");
 		skill1 = PlayerPrefs.GetString("NowEquipSkill1", "Grapple");
 		skill2 = PlayerPrefs.GetString("NowEquipSkill2", "Dash");
 
-		GameObject.Instantiate((GameObject)Resources.Load("Prefabs/Equipment/Weapon/" + weapon));
-		GameObject.Instantiate((GameObject)Resources.Load("Prefabs/Equipment/Skill/" + skill1));
-		GameObject.Instantiate((GameObject)Resources.Load("Prefabs/Equipment/Skill/" + skill2));
-		Debug.Log(weapon + " " + skill1 + " " + skill2);
+		weaponObj = GameObject.Instantiate((GameObject)Resources.Load("Prefabs/Equipment/Weapon/" + weapon));
+		skill1Obj = GameObject.Instantiate((GameObject)Resources.Load("Prefabs/Equipment/Skill/" + skill1));
+		skill2Obj = GameObject.Instantiate((GameObject)Resources.Load("Prefabs/Equipment/Skill/" + skill2));
+
+		skill1Obj.GetComponent<Skill>().actionKey = "Fire2";
+		skill2Obj.GetComponent<Skill>().actionKey = "Fire3";
 	}
 }
