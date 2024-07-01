748ef90473dd4f11b14efb235bcccb259c778804
void2610 <izumisan2610@gmail.com>
2024-07-02 03:18:41 +0900

    jet

---
+++ Assets/Scripts/Equipment/Jet.cs
@@ -0,0 +1,26 @@
+namespace NEquipment
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using NCharacter;
+
+    public class Jet : Equipment
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+            equipmentName = "Jet";
+            coolTimeLength = 10f;
+            activeTimeLength = 3f;
+        }
+
+        protected override void Effect()
+        {
+            base.Effect();
+            Vector2 now = player.GetComponent<Rigidbody2D>().velocity;
+            player.GetComponent<Rigidbody2D>().velocity = Vector2.Lerp(now, new Vector2(now.x, 15), 0.1f);
+        }
+    }
+}
