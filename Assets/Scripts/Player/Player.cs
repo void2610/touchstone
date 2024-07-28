78d6edb22607f00b2baa5bf30e140a319e2eebae
void2610 <izumisan2610@gmail.com>
2024-07-28 20:51:49 +0900

    fix jump reset raycast

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -151,8 +151,9 @@ namespace NCharacter
 				rb.constraints = RigidbodyConstraints2D.FreezeAll;
 			}
 
-			RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 2f, LayerMask.GetMask("Ground"));
-			if (hit.collider != null)
+			RaycastHit2D hit1 = Physics2D.Raycast(transform.position + new Vector3(0.7f, 0, 0), Vector2.down, 2f, LayerMask.GetMask("Ground"));
+			RaycastHit2D hit2 = Physics2D.Raycast(transform.position + new Vector3(-1.0f, 0, 0), Vector2.down, 2f, LayerMask.GetMask("Ground"));
+			if (hit1.collider != null || hit2.collider != null)
 			{
 				jumpCnt = 0;
 			}
