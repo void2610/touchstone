317a24d47bf2cbbadbc62d933c2cf1ce8aa044fe
void2610 <izumisan2610@gmail.com>
2024-08-04 22:59:51 +0900

    tween whole title button

--- Assets/Scripts/UI/TweenButton.cs
+++ Assets/Scripts/UI/TweenButton.cs
@@ -7,10 +7,18 @@ namespace NUI
 
     public class TweenButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
+        [Header("Button Settings")]
+        [SerializeField]
+        private bool tweenByPointer = true;
+        [SerializeField]
+        private bool tweenByClick = true;
+
+        [Header("Tween Settings")]
         [SerializeField]
         private float scale = 1.1f;
         [SerializeField]
         private float duration = 0.5f;
+
         private float defaultScale = 1.0f;
         private void OnClick()
         {
@@ -19,19 +27,31 @@ namespace NUI
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            this.transform.DOScale(defaultScale * scale, duration).SetEase(Ease.OutElastic);
+            if (tweenByPointer)
+                this.transform.DOScale(defaultScale * scale, duration).SetEase(Ease.OutElastic);
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
-            this.transform.DOScale(defaultScale, duration).SetEase(Ease.OutElastic);
+            if (tweenByPointer)
+                this.transform.DOScale(defaultScale, duration).SetEase(Ease.OutElastic);
         }
 
         private void Awake()
         {
             defaultScale = this.transform.localScale.x;
-            // Add Event Listener for button click
-            this.GetComponent<Button>().onClick.AddListener(OnClick);
+            if (tweenByClick)
+            {
+                if (this.GetComponent<Button>() != null)
+                {
+                    this.GetComponent<Button>().onClick.AddListener(OnClick);
+                }
+            }
+        }
+
+        private void Start()
+        {
+            defaultScale = this.transform.localScale.x;
         }
     }
 }
