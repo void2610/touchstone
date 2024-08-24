95275521165dcae1ef306a51f83f2cbbdbcdf951
void2610 <izumisan2610@gmail.com>
2024-08-25 01:42:34 +0900

    fix spike

--- Assets/Scripts/Map/Spike.cs
+++ Assets/Scripts/Map/Spike.cs
@@ -6,13 +6,15 @@ namespace NMap
 
     public class Spike : MonoBehaviour
     {
+        [SerializeField]
+        private bool isUp = true;
         private void OnTriggerEnter2D(Collider2D collision)
         {
             var p = collision.gameObject.GetComponent<Player>();
             if (p != null && !p.isInvincible && p.isMovable)
             {
                 p.CutHp(1);
-                p.JumpByEnemy(1.5f);
+                p.AddForce(new Vector2(0, isUp ? 26.25f : -26.25f));
             }
         }
     }
